3d27eb0aff2db1b2cd2d12cf7f6c04069a4e67ac
The-Lup <joanantonio95@gmail.com>
2025-11-03 22:07:56 -0400

    Code update v1.0.1

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,3 +1,52 @@
+import { addHours, format, getDay, parse, startOfWeek } from 'date-fns';
+import { enUS } from 'date-fns/locale/en-US';
+import { Calendar, dateFnsLocalizer, Views } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+import { useState } from 'react';
+import { Navbar } from '../';
+const locales = {
+  'en-US': enUS,
+};
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+
+const events = [
+  {
+    title: 'My birthday',
+    notes: 'Need buy a cake',
+    start: new Date(),
+    end: addHours(new Date(), 2),
+    bgcolor: '#fafafa',
+    user: {
+      _id: '123',
+      name: 'Lup',
+    },
+  },
+];
+
 export const CalendarPage = () => {
-  return <div>CalendarPage</div>;
+  const [currentView, setCurrentView] = useState(Views.MONTH);
+  const [currentDate, setCurrentDate] = useState(new Date());
+  return (
+    <>
+      <Navbar />
+      <Calendar
+        localizer={localizer}
+        date={currentDate}
+        view={currentView}
+        onView={setCurrentView}
+        onNavigate={setCurrentDate}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc(100vh - 80px)' }}
+      />
+    </>
+  );
 };
