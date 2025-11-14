0f343c4b22df00401376d3e27cd3b4c17be67805
The-Lup <joanantonio95@gmail.com>
2025-11-13 22:03:57 -0400

    code update 1.0.9

---
+++ src/store/auth/authSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const authSlice = createSlice({
+  name: 'second',
+  initialState: {
+    status: 'checking', //'authenticated', 'not-authenticated'
+    user: {},
+    errorMessage: undefined,
+  },
+
+  reducers: {
+    onChecking: (state) => {
+      state.status = 'checking';
+      state.user = {};
+      state.errorMessage = undefined;
+    },
+
+    onLogin: (state, { payload }) => {
+      state.status = 'authenticated';
+      state.user = payload;
+      state.errorMessage = undefined;
+    },
+  },
+});
+
+export const { onChecking, onLogin } = authSlice.actions;
